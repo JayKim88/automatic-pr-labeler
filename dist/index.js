69072f24aa18c1cc60fc2bdd3b3033d0d7ade97f
Jay Kim <mearak43@gmail.com>
2022-11-03 16:57:09 +0900

    add label update on time term basis

--- dist/index.js
+++ dist/index.js
@@ -12898,14 +12898,17 @@ async function run() {
   const { context = {} } = github;
   const { pull_request } = context.payload;
 
-  const prList = await octokit.request("GET /repos/{owner}/{repo}/pulls", {
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-  });
+  const prList = await octokit
+    .request("GET /repos/{owner}/{repo}/pulls", {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    })
+    .then((v) => v.data);
 
-  console.log("prListprListprList", prList);
+  const prIssuesNeedLabelUpdate =
+    !prList.length && prList.filter((v) => !v.draft);
 
-  if (!!pull_request.number) {
+  if (!!pull_request?.number) {
     await octokit.request(
       "POST /repos/{owner}/{repo}/issues/{issue_number}/labels",
       {
@@ -12921,14 +12924,36 @@ async function run() {
       issue_number: pull_request.number,
       body: "New Pull Request is waiting for you valuable Code-Review 🥰",
     });
-  } else {
-    console.log("This is Scheduled action");
-
-    const prList = await octokit.request("GET /repos/{owner}/{repo}/pulls", {
-      owner: context.repo.owner,
-      repo: "JayKim88/automatic-pr-labeler",
-    });
+    // return;
   }
+  if (!prIssuesNeedLabelUpdate) return;
+
+  // const checkedPrIssues = prIssuesNeedLabelUpdate.map(v => ({
+  //   issueNumber: v.number
+  //   lastLabels: v.labels
+  // }))
+
+  console.log("prListprListprList", prIssuesNeedLabelUpdate);
+
+  const updateDDayLabelStatus = async (v) => {
+    const prevDDayLabel = v.labels.filter((v) => v[0] === "D")[0];
+    if (!prevDDayLabel) return;
+    const newDDay = Number(prevDDayLabel.slice(-1)) - 1;
+    const newDDayResult = newDDay >= 0 ? newDDay : 0;
+    const newDDayLabel = "D-" + newDDayResult;
+
+    await octokit.request(
+      "PUT /repos/{owner}/{repo}/issues/{issue_number}/labels",
+      {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: v.number,
+        labels: [newDDayLabel],
+      }
+    );
+  };
+
+  await prIssuesNeedLabelUpdate.forEach((v) => updateDDayLabelStatus(v));
 }
 
 run();
