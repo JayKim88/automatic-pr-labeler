e3712de89538c7f4756919703beafc0387e99dfb
Jay Kim <mearak43@gmail.com>
2022-11-06 22:12:41 +0900

    fix: add logic to maintain prev labels not d-day label

--- src/action.js
+++ src/action.js
@@ -8,9 +8,9 @@ async function runAutomaticLabeler() {
   const { pull_request } = context.payload;
 
   if (!!pull_request?.number) {
-    // pr label 중 D로 시작하는 라벨이 존재하면, 안 만든다.
-    // 존재하지 않으면, D-5 로 만들어 준다.
-    const isDDayLabelExist = pull_request.labels.find((v) => v.name[0] === "D");
+    const prevLabels = pull_request.labels.map((v) => v.name);
+    const isDDayLabelExist = prevLabels.find((v) => v[0] === "D");
+
     if (isDDayLabelExist) return;
     await octokit.request(
       "POST /repos/{owner}/{repo}/issues/{issue_number}/labels",
@@ -18,7 +18,7 @@ async function runAutomaticLabeler() {
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: pull_request.number,
-        labels: ["D-5"],
+        labels: ["D-5", ...prevLabels],
       }
     );
     // return;
@@ -35,17 +35,20 @@ async function runAutomaticLabeler() {
     prList.length && prList.filter((v) => !v.draft);
 
   if (!prIssuesNeedLabelUpdate) return;
-  console.log("prIssuesNeedLabelUpdate", prIssuesNeedLabelUpdate);
+
   const updateDDayLabelStatus = async (v) => {
-    const prevDDayLabels = v.labels.filter((v) => v.name[0] === "D");
+    const prevLabels = v.labels.map((v) => v.name);
+    const prevDDayLabels = prevLabels.filter((v) => v[0] === "D");
     const minDay = Math.min(
       ...prevDDayLabels.map((v) => Number(v.name.slice(-1)))
     );
     const shortestDDayLabel = prevDDayLabels.find(
       (v) => Number(v.name.slice(-1)) === minDay
     );
-    console.log("shortestDDayLabel", shortestDDayLabel);
+
     if (!shortestDDayLabel) return;
+    const labelsExceptDDay = prevLabels.filter((v) => v[0] !== "D");
+
     const newDDay = Number(shortestDDayLabel.name.slice(-1)) - 1;
     const newDDayResult = newDDay >= 0 ? newDDay : 0;
     const newDDayLabel = "D-" + newDDayResult;
@@ -55,7 +58,7 @@ async function runAutomaticLabeler() {
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: v.number,
-        labels: [newDDayLabel],
+        labels: [newDDayLabel, ...labelsExceptDDay],
       }
     );
   };
